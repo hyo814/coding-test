33eb0e45e880af1b60545419a1e075df812038c7
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2024-06-12 11:30:43 +0900

    [level 2] Title: 주식가격, Time: 6.09 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,20 @@
+function solution(prices) {
+  const answer = new Array(prices.length).fill(0);
+  const stack = [];
+  let length = prices.length;
+
+  for(let i = 0; i < length; i++) {
+    while(stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+      let temp = stack.pop();
+      answer[temp] = i - temp;
+    }
+    stack.push(i);
+  }
+
+  while(stack.length) {
+    let temp = stack.pop();
+    answer[temp] = length - temp - 1;
+  }
+
+  return answer;
+}
