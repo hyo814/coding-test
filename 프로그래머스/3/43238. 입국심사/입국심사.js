157d989c30c28f02deff200939056d237f16ae6e
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2024-09-30 03:31:37 +0900

    [level 3] Title: 입국심사, Time: 102.95 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,19 @@
+function solution(n, times) {
+    let left = 1;
+    let right = Math.max(...times) * n;
+    let answer = right;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const sum = times.reduce((acc, time) => acc + Math.floor(mid / time), 0);
+        
+        if (sum >= n) {
+            answer = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return answer;
+}
