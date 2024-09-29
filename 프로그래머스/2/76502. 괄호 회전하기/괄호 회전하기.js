92f7363488c6d172181f921613cd4975675e9bd2
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2024-09-29 19:47:14 +0900

    [level 2] Title: 괄호 회전하기, Time: 109.28 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  let answer = 0;
+  for (let i = 0; i < s.length; i++) {
+    const arr = [];
+    const temp = i === 0 ? s : s.slice(i) + s.slice(0, i);
+    for (let j = 0; j < temp.length; j++) {
+      if (arr[arr.length - 1] === '(' && temp[j] === ')') arr.pop();
+      else if (arr[arr.length - 1] === '[' && temp[j] === ']') arr.pop();
+      else if (arr[arr.length - 1] === '{' && temp[j] === '}') arr.pop();
+      else arr.push(temp[j]);
+    }
+    if (arr.length === 0) answer++;
+  }
+  return answer;
+}
